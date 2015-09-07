1ab2fd4b7bd39dc7c91301159f73dd6e8324f547
zpr <zepedrojk@gmail.com>
2015-09-07 02:37:27 +0100

    tests

--- src/app/add/add.controller.js
+++ src/app/add/add.controller.js
@@ -2,5 +2,20 @@
 
 angular.module('YaDespesas')
   .controller('AddCtrl', function($scope) {
-    $scope.hello = 'Hello Add Tab';
+    //Init
+    $scope.who = ['Zé','Susana'];
+    $scope.expensiveTypes = ['Individual', 'Colectiva'];
+    // $scope.percentageTypes = ['50-50', '70-30'];
+    $scope.descriptions = ['Renda', 'Internet', 'Água', 'Luz', 'Gás', 'Alimentação', 'Casa', 'Lazer', 'Outros'];
+
+    $scope.whoSelected = $scope.who[0];
+    $scope.expensiveTypeSelected = $scope.expensiveTypes[1];
+    // $scope.percentageTypeSelected = $scope.percentageTypes[0];
+    // $scope.descriptionSelected =  $scope.descriptions[1];
+
+    $scope.Send = function() {
+        console.log('Sending Data');
+        // delete $scope.descriptionSelected;
+        $scope.descriptionSelected = 'Luz';
+    }
   });
