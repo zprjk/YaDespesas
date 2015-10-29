aecc885fcc5f6ab6d542b647023e2f42a64d8ff3
Ze Pedro Rocha <zepedrojk@gmail.com>
2015-10-29 15:14:14 +0000

    clean data

--- server/data/GET_MonthValues.sql
+++
@@ -1,6 +0,0 @@
-SELECT user.name, year_id, month_id, expenseType.name, value, description, date 
-FROM Expense AS expense, User AS user, ExpensiveType AS expenseType  
-WHERE user.id=expense.user_id 
-AND expense.expenseType_id=expenseType.id 
-AND expense.month_id=8 
-AND expense.year_id=2015
